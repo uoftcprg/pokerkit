02375eb89d4c2fc293015105355d84c4b30dfd31
Juho <juho-kim@outlook.com>
2021-02-20 20:01:14 -0500

    internal improvements

--- pokertools/hands.py
+++ pokertools/hands.py
@@ -1,4 +1,4 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from collections import Hashable
 from functools import total_ordering
 from typing import Any
@@ -10,27 +10,40 @@ from pokertools.cards import Card
 @total_ordering
 class Hand(Hashable, ABC):
     """Hand is the base class for all hands."""
-    _lookup: Lookup
-
-    def __init__(self, *cards: Card):
-        self.__index = self._lookup.index(*cards)
 
     def __lt__(self, other: Any) -> bool:
         if isinstance(other, Hand):
-            return self.__index > other.__index
+            return self.index > other.index
         else:
             return NotImplemented
 
     def __eq__(self, other: Any) -> bool:
         if isinstance(other, Hand):
-            return self.__index == other.__index
+            return self.index == other.index
         else:
             return NotImplemented
 
     def __hash__(self) -> int:
-        return hash(self.__index)
+        return hash(self.index)
+
+    @property
+    @abstractmethod
+    def index(self) -> int:
+        pass
 
 
-class StandardHand(Hand):
+class LookupHand(Hand):
+    """LookupHand is the abstract base class for all lookup hands."""
+    _lookup: Lookup
+
+    def __init__(self, *cards: Card):
+        self.__index = self._lookup.index(*cards)
+
+    @property
+    def index(self) -> int:
+        return self.__index
+
+
+class StandardHand(LookupHand):
     """StandardHand is the base class for all standard hands."""
     _lookup = StandardLookup()
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst', 'r') as long_description_file:
 
 setup(
     name='pokertools',
-    version='0.0.1a3',
+    version='0.0.1b1',
     author='Juho Kim',
     author_email='juho-kim@outlook.com',
     description='A Python package for various poker tools',
