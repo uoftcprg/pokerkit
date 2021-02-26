02cdacdbf02c82fd38d8883af784b36109a638a5
Juho <juho-kim@outlook.com>
2021-02-26 13:59:15 -0500

    using general types

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst', 'r') as long_description_file:
 
 setup(
     name='pokertools',
-    version='0.0.2.dev11',
+    version='0.0.2.dev12',
     author='Juho Kim',
     author_email='juho-kim@outlook.com',
     description='A Python package for various poker tools',
