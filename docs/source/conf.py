b1cf1b698cc1e73e41ba6685f6f7ea9c0ac35e71
Juho <juho-kim@outlook.com>
2021-03-06 22:39:44 -0500

    frozenset is better

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Juho Kim'
 author = 'Juho Kim'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.2a1'
+release = '0.0.2a2'
 
 # -- General configuration ---------------------------------------------------
 
