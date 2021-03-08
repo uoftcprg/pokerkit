2ee7ad971f2b5cbb8fde9e00366afc7cf8a5774d
Juho <juho-kim@outlook.com>
2021-03-08 17:43:09 -0500

    Finalized 0.0.2

--- pokertools/tests/test_cards.py
+++ pokertools/tests/test_cards.py
@@ -1,11 +1,11 @@
 from unittest import main
 
-from auxiliary.tests import ExtendedTestCase
+from auxiliary.tests import ExtTestCase
 
 from pokertools import HoleCard, parse_card, parse_cards, suited
 
 
-class CardTestCase(ExtendedTestCase):
+class CardTestCase(ExtTestCase):
     def test_cards(self) -> None:
         self.assertEqual(repr(parse_card('4h')), '4h')
         self.assertEqual(str(parse_card('4h')), '4h')
