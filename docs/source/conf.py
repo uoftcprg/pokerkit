1e14ebb82a30638617471ddbd3fef0d2edf090f9
Juho <juho-kim@outlook.com>
2021-03-09 12:05:32 -0500

    prefer tuple over list

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Juho Kim'
 author = 'Juho Kim'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.4.dev1'
+release = '0.0.3.dev1'
 
 # -- General configuration ---------------------------------------------------
 
