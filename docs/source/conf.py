86831337bdd78ecea19444fef8c02c03bf1f3986
Juho <juho-kim@outlook.com>
2021-03-11 22:57:34 -0500

    Refactored names and files for descriptiveness

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Juho Kim'
 author = 'Juho Kim'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.3.dev4'
+release = '0.0.3.dev5'
 
 # -- General configuration ---------------------------------------------------
 
