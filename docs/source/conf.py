e475966525e9f1b6f905f859c83e3e91b567d8df
Juho <juho-kim@outlook.com>
2021-03-11 20:30:44 -0500

    made slight optimization

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Juho Kim'
 author = 'Juho Kim'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.3.dev3'
+release = '0.0.3.dev4'
 
 # -- General configuration ---------------------------------------------------
 
