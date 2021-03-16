d529d2241e492173bdd69ca85b8d50b9e4c73637
Juho <juho-kim@outlook.com>
2021-03-16 14:17:54 -0400

    modified lt signature

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Juho Kim'
 author = 'Juho Kim'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.3.dev10'
+release = '0.0.3.dev11'
 
 # -- General configuration ---------------------------------------------------
 
