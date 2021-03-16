735a29aa57031e32e975beb7700428624f3eadb7
Juho <juho-kim@outlook.com>
2021-03-16 19:36:03 -0400

    optimized code

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Juho Kim'
 author = 'Juho Kim'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.3.dev11'
+release = '0.0.3.dev12'
 
 # -- General configuration ---------------------------------------------------
 
