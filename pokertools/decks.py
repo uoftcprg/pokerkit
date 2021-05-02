a709b3bb84e9f9cb51bb87c1b422c61dc5894c6a
juho <juho-kim@outlook.com>
2021-05-01 21:24:31 -0400

    Deck inherits from set

--- pokertools/decks.py
+++ pokertools/decks.py
@@ -1,14 +1,10 @@
-from collections.abc import Collection, Iterable, Iterator
-from typing import Any
+from collections.abc import Iterable
 
 from pokertools.cards import Card, SHORT_RANKS, STANDARD_RANKS, Suit
 
 
-class Deck(Collection[Card]):
+class Deck(set[Card]):
     """Deck is the class for decks."""
-
-    def __init__(self, cards: Iterable[Card]):
-        self.__cards = set(cards)
 
     def draw(self, cards: Iterable[Card]) -> None:
         """Draws the cards from this deck.
@@ -16,16 +12,7 @@ class Deck(Collection[Card]):
         :param cards: The cards to be drawn.
         :return: None.
         """
-        self.__cards -= set(cards)
-
-    def __iter__(self) -> Iterator[Card]:
-        return iter(self.__cards)
-
-    def __contains__(self, __x: Any) -> bool:
-        return __x in self.__cards
-
-    def __len__(self) -> int:
-        return len(self.__cards)
+        self.__isub__(set(cards))
 
 
 class StandardDeck(Deck):
