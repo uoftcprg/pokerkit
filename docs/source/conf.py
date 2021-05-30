c3d17fd60a78be82543a527c62af672bcbb1ac58
Juho <juho-kim@outlook.com>
2021-05-30 15:49:36 -0400

    using built-in function

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Juho Kim'
 author = 'Juho Kim'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.3.dev26'
+release = '0.0.3.dev27'
 
 # -- General configuration ---------------------------------------------------
 
