a6a82653e1b3fae0122296697fd8eca3b2dbdaec
Juho <juho-kim@outlook.com>
2021-07-10 14:00:40 -0400

    Added some greek hold'em

--- pokertools/tests/test_games/test_greek_hold_em.py
+++ pokertools/tests/test_games/test_greek_hold_em.py
@@ -1,6 +1,6 @@
 from unittest import TestCase, main
 
-from pokertools import FixedLimitGreekHoldEm, NoLimitGreekHoldEm, PotLimitGreekHoldEm
+from pokertools import FixedLimitGreekHoldEm, NoLimitGreekHoldEm, PotLimitGreekHoldEm, Stakes
 from pokertools.tests import PokerTestCaseMixin
 
 
@@ -8,21 +8,53 @@ class FixedLimitGreekHoldEmTestCase(PokerTestCaseMixin, TestCase):
     GAME_TYPE = FixedLimitGreekHoldEm
 
     def test_hands(self):
-        ...  # TODO
+        self.assert_terminal_poker_game(self.GAME_TYPE(Stakes(1, (2, 5), small_bet=10), (100, 100)).parse(
+            'dh 0 AhAc', 'dh 1 AsKs',
+            'br', 'cc',
+            'db KcQdJh',
+            'br', 'cc',
+            'db Tc',
+            'br', 'cc',
+            'db 9d',
+            'cc', 'cc',
+            's', 's',
+        ), (True, True), (54, 146))
 
 
 class PotLimitGreekHoldEmTestCase(PokerTestCaseMixin, TestCase):
     GAME_TYPE = PotLimitGreekHoldEm
 
     def test_hands(self):
-        ...  # TODO
+        self.assert_terminal_poker_game(self.GAME_TYPE(Stakes(1, (2, 5), small_bet=10), (100, 100)).parse(
+            'dh 0 AhAc', 'dh 1 AsKs',
+            'br', 'cc',
+            'db KcQdJh',
+            'br', 'cc',
+            'db Tc',
+            'br', 'cc',
+            'db 9d',
+            'cc', 'cc',
+            's', 's',
+        ), (True, True), (64, 136))
 
 
 class NoLimitGreekHoldEmTestCase(PotLimitGreekHoldEmTestCase, TestCase):
     GAME_TYPE = NoLimitGreekHoldEm
 
     def test_hands(self):
-        super().test_hands()  # TODO
+        super().test_hands()
+
+        self.assert_terminal_poker_game(self.GAME_TYPE(Stakes(1, (2, 5)), (100, 100)).parse(
+            'dh 0 AhAc', 'dh 1 AsKs',
+            'br', 'cc',
+            'db KcQdJh',
+            'br', 'cc',
+            'db Tc',
+            'br', 'cc',
+            'db 9d',
+            'cc', 'cc',
+            's', 's',
+        ), (True, True), (79, 121))
 
 
 if __name__ == '__main__':
